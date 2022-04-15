b36ac5de49d5391e4241ea441ee17656e3f3831a
danilo47 <danilo@jeraapps.com>
2022-04-15 14:31:46 -0300

    rotate logic test

--- app/src/main/java/com/example/appimc/ResultadoActivity.kt
+++ app/src/main/java/com/example/appimc/ResultadoActivity.kt
@@ -2,10 +2,21 @@ package com.example.appimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 class ResultadoActivity : AppCompatActivity() {
+   private lateinit var seta:ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+        seta = findViewById(R.id.seta)
+        rodar()
+    }
+
+    private fun rodar(){
+        val rodar = AnimationUtils.loadAnimation(this, R.anim.rodar_teste)
+        seta.animation = rodar
     }
 }
