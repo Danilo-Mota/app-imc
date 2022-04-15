a370e30c9f2c562be2b65d49b3c98b4b64e0dd77
danilo47 <danilo@jeraapps.com>
2022-04-15 20:39:28 -0300

    resolve npe

--- app/src/main/java/com/example/appimc/ResultadoActivity.kt
+++ app/src/main/java/com/example/appimc/ResultadoActivity.kt
@@ -10,31 +10,29 @@ class ResultadoActivity : AppCompatActivity() {
     private val calcular = Calcular()
     private lateinit var peso: String
     private lateinit var altura: String
-    private var resultadoText: TextView = findViewById(R.id.result)
-    private var btnCalc: MaterialButton = findViewById(R.id.calc_button)
+    private lateinit var resultadoText: TextView
+    //private lateinit var seta: ImageView = findViewById(R.id.seta)
 
-    //private lateinit var seta:ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
         onClick()
-        //   seta = findViewById(R.id.seta)
         // rodar()
     }
     private fun onClick(){
+        val btnCalc: MaterialButton = findViewById(R.id.calc_button)
         btnCalc.setOnClickListener {
             peso = findViewById<EditText>(R.id.peso_editText).text.toString()
             altura = findViewById<EditText>(R.id.altura_editText).text.toString()
+            resultadoText = findViewById(R.id.result)
 
             calcular.set(peso.toFloat(), altura.toFloat())
             resultadoText.text = calcular.get().toString()
         }
     }
 
-    private fun rodar(){
+   /* private fun rodar(){
         //   val rodar = AnimationUtils.loadAnimation(this, R.anim.rodar_teste)
         //   seta.animation = rodar
-    }
-
-
+    }*/
 }
