17aee0245b2e38ad9a1104685f14249f524b51f8
danilo47 <danilo@jeraapps.com>
2022-04-15 12:37:07 -0300

    intent for result

--- app/src/main/java/com/example/appimc/MainActivity.kt
+++ app/src/main/java/com/example/appimc/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.appimc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
-    private val calcular = Calcular()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        comecar()
+    }
+
+    private fun comecar(){
+        val comecar = findViewById<MaterialButton>(R.id.button)
+        val irParaResultadoActivity = Intent(this, ResultadoActivity::class.java)
+
+        comecar.setOnClickListener {
+            startActivity(irParaResultadoActivity)
+        }
     }
 }
